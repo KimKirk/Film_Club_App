6f7555625be56e9faa83e14f947bc5b6018e67ed
KeK <kimberleyEkirk@gmail.com>
2016-12-06 17:27:52 -0800

    add fetch data method add json data pull method add method body to getView method

--- FetchMovies.java
+++ FetchMovies.java
@@ -2,10 +2,19 @@ package com.spellflight.android.popularmovies;
 
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.annotation.VisibleForTesting;
 import android.util.Log;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URI;
+import java.net.ProtocolException;
 import java.net.URL;
 
 /**
@@ -13,7 +22,9 @@ import java.net.URL;
  */
 public class FetchMovies extends AsyncTask {
 
-    //TODO: 11/16/2016 finish watching how to create custom arrayadapter in Google+ https://plus.google.com/events/chlh8qqr5q5grs1lajpqnvvql8k?authkey=CNXMrZuHsMWhNg
+    private String lineOfText = null;
+    
+
 
     /* 1. extend AsyncTask
         2. you will have to override at least one method from the class
@@ -44,32 +55,119 @@ public class FetchMovies extends AsyncTask {
     //TODO: check if parameter type is accurate and return type is accurate
     protected Object doInBackground(Object[] objects) {
 
-        //TODO: check variables here to see if they need access modifier private or not
-        final String TAG = "URL error";
+        fetchDataFromServer();
+
+        //TODO: 11/18/2016 note in a README where it came from, so someone else trying to run your code can create their own key and will quickly know where to put it. Instructors and code reviewers will expect this behavior for any public GitHub code.
+    }
+
+
+
+
+    //gets data from json object
+    // DESIGN: 12/6/2016  take string that has data in json format, create json object and pass string to it,
+        // use json object that has json data and pass it into json array so it's easier to get json data out of array, once out of array put into ArrayList with just the data item from the json array that you want
+
+    public void getDataFromJson () {
+
+        /*Json object has a json array inside of it
+        json array is= results
+        element 0 = poster_path
+        element 1= poster_path*/
+
+        // bufferedreader holds json so need to create json object and json array to extract needed data
+        //pull data from the reader object into a json object
+
+
+        //DONE: 3rd - 11/18/2016 fetch the data/images from themoviedb.org need to start http request, need to stream data into program, do I need to create an array that holds the URL for each image? that Picasso then uses in the load() method?
+
+        //TODO: 2nd - after put data into json array figure out how to add to regular array so adapter can use the data? does adapter take json array?
+
+        // TODO: 2nd - 11/10/2016 create array that holds image data from moviedb server, hold in a variable, replace imageArray above with variable name
+
+        // DESIGN: 12/2/2016 add conditional statement so that you can choose which data to pull from the array so that you can make this method public and use the results in ImageAdapterView class 
+    }
+
+
+
+    //does the work normally inside of doInBackground but put the work into own method so is cleaner and easier to modify in future and won't affect doInBackground
+    //fetches data from the server
+    //TESTING: FULL METHOD PASSED
+    protected void fetchDataFromServer() throws Exception  {
+        //DONE: check variables here to see if they need access modifier private or not
         //DONE: add API key but make sure to check how to proceed when upload to GIT
+        final String badUrl = "check URL";
+        final String badProtocolRequest = "check setRequestMethod()";
+        final String badEncoding = "check InputStreamReader";
+        final String badConnectionObject = "check HttpURLConnection";
+        final String badIoException = "check connect() or readline()";
         //create URL object
         final String MOVIE_BASE_URL = "https://api.themoviedb.org/3/movie/popular?";
         final String API_KEY_PARAM = "api_key";
         final String LANGUAGE_PARAM = "language";
+        URL url = null;
+        BufferedReader reader;
+        InputStream inputStream = null;
+        HttpURLConnection connection = null;
+        Uri builtUri;
 
 
-        Uri builtUri = Uri.parse(MOVIE_BASE_URL).buildUpon()
-                .appendQueryParameter(API_KEY_PARAM, " ").appendQueryParameter(LANGUAGE_PARAM, "en-US" )
-                .build();
-
-        //DONE: add try/catch block to handle this malformed URL exception then error will be removed
+        //TESTING: PASSED
         try {
-            URL url = new URL(builtUri.toString());
-            System.out.println(url.toString());
+            //FIXME: key is 821d4fff9880f197021eaccba83fb04f
+            // FIXME: 12/2/2016 here is the final url that shows the data you will get from server https://api.themoviedb.org/3/movie/popular?&api_key=821d4fff9880f197021eaccba83fb04f&language=en-US
+
+            builtUri = Uri.parse(MOVIE_BASE_URL).buildUpon()
+                    .appendQueryParameter(API_KEY_PARAM, " ").appendQueryParameter(LANGUAGE_PARAM, "en-US")
+                    .build();
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException m) {
+            Log.d(badUrl, "doInBackground: ");
+            System.out.println(badUrl);
         }
-        catch (MalformedURLException m) {
-            Log.d(TAG, "doInBackground: ");
-            System.out.println(TAG);
+
+
+        //TESTING: PASSED
+        //opens the http connection and the stream, streams in data
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            inputStream = connection.getInputStream();
+
+            //bufferedreader is holding the data from the server
+            reader = new BufferedReader((new InputStreamReader(inputStream, "UTF-8")));
+
+            //take data in reader and convert to a string to be used as input to json object in other method
+            lineOfText = reader.readLine();
+
+            // DONE: 12/2/2016 check for condition that would create IOException, this will propogate the exception to another method with catch block for IOException
+            }
+        catch (ProtocolException pr) {
+            Log.d( badProtocolRequest , "fetchDataFromServer: ");
         }
+        catch (UnsupportedEncodingException uc) {
+            Log.d(badEncoding, "fetchDataFromServer: ");
+        }
+        catch (NullPointerException np) {
+            Log.d(badConnectionObject, "fetchDataFromServer: ");
+        }
+        catch (IOException io) {
+            Log.d(badIoException, "fetchDataFromServer: ");
 
-        //TODO: 11/18/2016 fetch the data/images from themoviedb.org need to start http request, need to stream data into program, do I need to create an array that holds the URL for each image? that Picasso then uses in the load() method?
 
-        //TODO: 11/18/2016 note in a README where it came from, so someone else trying to run your code can create their own key and will quickly know where to put it. Instructors and code reviewers will expect this behavior for any public GitHub code.
+        } finally {
+            try{
+                if (inputStream != null) {
 
-         }
+                    inputStream.close();
+                }
+            }
+            catch (IOException ioe) {
+                Log.d(badIoException, "doInBackground: ");
+            }
+            connection.disconnect();
+        }
+    }
 }
+
+
--- ImageAdapterView.java
+++ ImageAdapterView.java
@@ -1,27 +1,45 @@
 package com.spellflight.android.popularmovies;
 
 
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by Kim Kirk on 11/10/2016.
  */
 public class ImageAdapterView extends BaseAdapter {
+    //TODO: figure out why need to override getItemId() method
 
 
-    //holds arrayadapter
-    ArrayAdapter<ImageView> arrayAdapter = new ArrayAdapter<ImageView>(MyApplication.getAppContext(),android.R.layout.simple_gallery_item, imageArray);
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
 
-    // TODO: 11/10/2016 create array that holds image data from moviedb server, hold in a variable, replace imageArray above with variable name 
-    // TODO: 11/10/2016 follow along using this link for what to do next https://developer.android.com/guide/topics/ui/layout/gridview.html
-    // TODO: 11/18/2016 put data into arrayadapter for layout View to use need to use custom adapter to load images into the layout View, stick the Picasso code inside of the getView() method that you will override because Picasso creates the views for each of the data elements
+        //handling if there is no view object to put data into
+        if (convertView == null) {
+            convertView = View.inflate(MyApplication.getAppContext(), R.layout.activity_main, parent);
+        }
 
 
+        //position is used to get the current position of the data in the array/data structure...this returns an int so take the int and use arrayStructure[position] to extract data at that position in array
 
-    //DESIGN: 11/18/2016 should I put arrayadapter in new class then have background thread use objects from arrayadapter? or put arrayadapter inside background thread as inner class so all this is done on background thread?
+        //picasso will take each array data element and stick it into the view
+        //should load() have the current position in the array as its input (so get position in array and return the value there) then add the base URL to that result?
+        (ImageView) convertView = Picasso.with(MyApplication.getAppContext()).load(arrayAdapter[position]).into(convertView);
+
+        return convertView;
+    }
+
+    //DONE: add Picasso library to gradle
+
+    //DONE: - 11/16/2016 finish watching how to create custom arrayadapter in Google+ https://plus.google.com/events/chlh8qqr5q5grs1lajpqnvvql8k?authkey=CNXMrZuHsMWhNg
+
+    // DONE: - 11/10/2016 follow along using this link for what to do next to add arrayAdapter stuff into layout https://developer.android.com/guide/topics/ui/layout/gridview.html
+    // DONE:  - 11/18/2016 put data into arrayadapter for layout View to use need to use adapter to load images into the layout View, stick the Picasso code inside of the getView() method that you will override because Picasso creates the views for each of the data elements
+
 
 
 

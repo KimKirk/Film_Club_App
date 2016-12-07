f9086bb45478e0308611706740d75dcdc1df79a7
KeK <kimberleyEkirk@gmail.com>
2016-12-06 17:29:14 -0800

    add mainactivity with current changes

---
+++ MainActivity.java
@@ -0,0 +1,54 @@
+package com.spellflight.android.popularmovies;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GridView gridView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TESTING: NOT TESTED
+        // TODO: 12/2/2016 add name of array that holds data
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MyApplication.getAppContext(), R.layout.activity_main, arrayNamePlaceHolder);
+        gridView = (GridView) findViewById(R.id.grid_view);
+        gridView.setAdapter(arrayAdapter);
+
+        startTask();
+    }
+
+
+    //TESTING: PASSED
+    //checks network connection and starts background thread if connection is good
+    public void startTask() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if(networkInfo != null && networkInfo.isConnected()) {
+            new FetchMovies().execute();
+            // TODO: 12/2/2016 remove line below
+            Log.d("Network is up", "startTask: ");
+        } else {
+            Toast toast = new Toast(MyApplication.getAppContext());
+            toast.makeText(MyApplication.getAppContext(),R.string.network_check,Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+    }
+
+    //TODO: 11/18/2016 ADD CODE THAT SAYS This product uses the TMDb API but is not endorsed or certified by TMDb. with their logo see this page https://www.themoviedb.org/about/logos-attribution
+
+
+
+}

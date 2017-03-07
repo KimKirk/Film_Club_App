74b15377444b17da9640a184fcb344c5bba94212
KeK <kimberleyEkirk@gmail.com>
2017-03-07 10:33:25 -0800

    remove retrieve data from Bundle in DetailFragment remove newInstance() use from DetailHostActivity add onSaveInstanceState() to DetailHostActivity

--- DetailFragment.java
+++ DetailFragment.java
@@ -13,7 +13,8 @@ import android.widget.TextView;
  */
 public class DetailFragment extends Fragment{
 
-    // TODO: 3/1/2017 test this method to make sure it works correctly 
+    // DONE: 3/1/2017 test this method to make sure it works correctly
+    // FIXME: 3/6/2017 this method does not work when you save the fragment instance inside host activity
     //this is a way to set the Fragment's fields so that you don't use the default constructor and then have to call setters
     public static DetailFragment newInstance(Parcelable movDetParcel){
         //create new instance of detail fragment
@@ -24,6 +25,7 @@ public class DetailFragment extends Fragment{
         arguments.putParcelable("movieDetailsParcel",movDetParcel);
         //set arguments/field for the Detail Fragment
         detailFrag.setArguments(arguments);
+        //outer bundle = arguments, bundle, parcelable
         //return the new instance with the set field to the caller
         return detailFrag;
     }
@@ -34,9 +36,15 @@ public class DetailFragment extends Fragment{
 
     }
 
+    //DESIGN: possibly override and implement onActivityCreated() where you will set the text for the Fragment's View using the data you received from Intent; do this in this method because need to make sure the View has been initialized for the Fragment before adding text to it
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
+        //restore data for fragment's text views here because when activity is created it means can access the data the activity shares with fragment
+        /*if(savedInstanceState != null){
+            //get data from Bundle
+            //set text for text views
+        }*/
         // DONE: 2/14/2017  get the data from the new instance of Detail Fragment
         // DONE: 2/14/2017  set the data on the views that will show the data
         TextView titleView = (TextView)getActivity().findViewById(R.id.original_title);
@@ -52,12 +60,7 @@ public class DetailFragment extends Fragment{
         releaseDateView.setText(movieDetailsParcel.getRelease());
         voteView.setText(movieDetailsParcel.getVote());
 
-
-
     }
-
-    //DESIGN: possibly override and implement onActivityCreated() where you will set the text for the Fragment's View using the data you received from Intent; do this in this method because need to make sure the View has been initialized for the Fragment before adding text to it
-
 
 
     @Override
@@ -66,17 +69,11 @@ public class DetailFragment extends Fragment{
         return inflater.inflate(R.layout.detail_fragment,container, false);
     }
 
-    @Override
-    public void onPause(){
-        //when Fragment is paused and it might be stopped, save the Views and any data you wish to use to recreate the Fragment instance here by sticking the data inside a Bundle
-            //onPause will call onSavedInstanceState() so override this and save your stuff there
-
-        super.onPause();
-    }
-
+    //saves the data from the fragment's textviews
     /*@Override
     public void onSavedInstanceState(Bundle outState){
         //need to save text of TextViews
+
 
         //call super to save default View values too
         super.onSaveInstanceState(outState);
--- DetailHostActivity.java
+++ DetailHostActivity.java
@@ -3,6 +3,7 @@ package com.spellflight.android.popularmovies;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 
 /**
@@ -10,8 +11,10 @@ import android.support.v4.app.FragmentActivity;
  */
 public class DetailHostActivity extends FragmentActivity {
     private DetailFragment detailFragment = new DetailFragment();
+    private DetailFragment fragment;
 
-    // TODO: 3/1/2017 test this class to make sure the onCreate() runs correctly 
+
+    // DONE: 3/1/2017 test this class to make sure the onCreate() runs correctly 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +22,13 @@ public class DetailHostActivity extends FragmentActivity {
         setContentView(R.layout.detail_fragment_container);
 
         if(savedInstanceState != null) {
+            //recreates fragment instance
+            fragment = (DetailFragment)getSupportFragmentManager().getFragment(savedInstanceState,"fragment");
             //use newInstance() to recreate the new fragment with the correct data in it
-            DetailFragment newDetailFragmentInstance = detailFragment.newInstance(getIntent().getExtras());
-            getSupportFragmentManager().beginTransaction().replace(R.id.detail_container,newDetailFragmentInstance).commit();
+            // TODO: 3/7/2017 figure out why this did not work correctly? fragment saved already had argument with data, so no need to create again in newInstance() 
+            //DetailFragment newDetailFragmentInstance = fragment.newInstance(getIntent().getExtras());
+            //replaces any current fragment with new fragment instance
+            getSupportFragmentManager().beginTransaction().replace(R.id.detail_container,fragment).commit();
         }
         else {
             //set arguments for Fragment--this allows you to pass data into the newly created Fragment so it has the data you pass to it, this is for the first time the fragment is created
@@ -32,5 +39,14 @@ public class DetailHostActivity extends FragmentActivity {
         }
     }
 
-    // TODO: 2/3/2017  save the instance of the Movie Details Fragment so that when the user changes orientation of the screen will get same data as before to make it seem as if nothing has changed, if so use code in SettingsHostActivity to save fragment instance and recreate it when onCreate() is called again
+    // DONE: 2/3/2017  save the instance of the Movie Details Fragment so that when the user changes orientation of the screen will get same fragment instance as before to make it seem as if nothing has changed, if so use code in SettingsHostActivity to save fragment instance and recreate it when onCreate() is called again
+    //// DONE: 3/6/2017 this does not save the fragment, fragment is not put into bundle
+    // FIXME: 3/7/2017 fragment is not being saved during second device rotation because there is no fragment in the fragment manager?
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        Log.d("fragmentManagerState", "onSaveInstanceState: " + getSupportFragmentManager().getFragments());
+        //saves fragment instance before fragment is killed
+        getSupportFragmentManager().putFragment(outState,"fragment",detailFragment);
+        super.onSaveInstanceState(outState);
+    }
 }

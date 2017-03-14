d5b3e08f4e634c9fec4a2e4b7c4e500f09be59f5
KeK <kimberleyEkirk@gmail.com>
2017-03-14 09:28:53 -0700

    add final comments to SettingsHostActivity

--- SettingsHostActivity.java
+++ SettingsHostActivity.java
@@ -8,27 +8,33 @@ import android.support.v4.app.FragmentActivity;
  */
 public class SettingsHostActivity extends FragmentActivity {
 
+    //holds the returned value of FragmentManager
     private android.app.Fragment mFragment;
     //holds a newly created SettingsFragment instance
     private SettingsFragment mSettingsFragment = new SettingsFragment();
 
 
+    //sets the content view of the Fragment inside of the host Activity's view hierarchy
     //TESTING: PASSED
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //gets the saved instance View data inside the Bundle and does default creation of setting saved View data onto that View
         super.onCreate(savedInstanceState);
+        //sets the content view container for the Fragment inside of the host Activity's view hierarchy
         setContentView(R.layout.fragment_container);
 
         //if bundle has data in it replace current mFragment with previous mFragment instance
         //when mFragment is recreated it will have previous instance's data
         if(savedInstanceState != null){
+            //get the Fragment stored inside the Bundle of savedInstanceState
             mFragment = getFragmentManager().getFragment(savedInstanceState,"mFragment");
+            //start transaction to replace any current Fragment with Fragment inside of mFragment
             getFragmentManager().beginTransaction().replace(R.id.container , mFragment).commit();
         }
-        else {
+        else {//if the Bundle sent into onCreate method is empty then just attach the Settings Fragment to the host Activity
             //// DONE: 1/5/2017 this is not working, need to figure out how to add the mFragment
             //add Fragment to SettingHostActivity that shows Settings Menu
-            //use getFragmentManager because you are using a Preference Fragment via SettingsFragment in add()
+            //use getFragmentManager because you are using a Preference Fragment via SettingsFragment in add(), Preference Fragment does not support supportFragmentManager because Preference Fragment created before Fragments existed in Android
             getFragmentManager().beginTransaction().add(R.id.container, mSettingsFragment).commit();
         }
 
@@ -39,7 +45,7 @@ public class SettingsHostActivity extends FragmentActivity {
     //puts mFragment instance into the bundle, saves the mFragment instance so that when it needs to be recreated host activity can retrieve saved mFragment instance
     @Override
     protected void onSaveInstanceState(Bundle outState){
-        //gets the support fragment manager, puts current fragment instance that is found by the support fragment manager into the Bundle "outstate"
+        //gets the fragment manager, puts current fragment instance that is found by the fragment manager into the Bundle "outstate"
         getFragmentManager().putFragment(outState,"mFragment",getFragmentManager().findFragmentById(R.id.container));
         //saves Bundle
         super.onSaveInstanceState(outState);

de024f6b233b140fba7d7178092cf7b502497ecd
KeK <kimberleyEkirk@gmail.com>
2017-03-15 09:27:31 -0700

    refactor variables for DetailHostActivity

--- DetailHostActivity.java
+++ DetailHostActivity.java
@@ -9,8 +9,8 @@ import android.support.v4.app.FragmentActivity;
  * Created by Kim Kirk on 2/3/2017.
  */
 public class DetailHostActivity extends FragmentActivity {
-    private DetailFragment detailFragment = new DetailFragment();
-    private DetailFragment fragment;
+    private DetailFragment mDetailFragment = new DetailFragment();
+    private DetailFragment mFragment;
 
 
     // DONE: 3/1/2017 test this class to make sure the onCreate() runs correctly 
@@ -21,31 +21,31 @@ public class DetailHostActivity extends FragmentActivity {
         setContentView(R.layout.detail_fragment_container);
 
         if(savedInstanceState != null) {
-            //recreates fragment instance
-            fragment = (DetailFragment)getSupportFragmentManager().getFragment(savedInstanceState,"fragment");
-            //use newInstance() to recreate the new fragment with the correct data in it
-            // DONE: 3/7/2017 fragment saved already had argument with data, so no need to create again in newInstance()
-            //DetailFragment newDetailFragmentInstance = fragment.newInstance(getIntent().getExtras());
-            //replaces any current fragment with new fragment instance
-            getSupportFragmentManager().beginTransaction().replace(R.id.detail_container,fragment).commit();
+            //recreates mFragment instance
+            mFragment = (DetailFragment)getSupportFragmentManager().getFragment(savedInstanceState,"mFragment");
+            //use newInstance() to recreate the new mFragment with the correct data in it
+            // DONE: 3/7/2017 mFragment saved already had argument with data, so no need to create again in newInstance()
+            //DetailFragment newDetailFragmentInstance = mFragment.newInstance(getIntent().getExtras());
+            //replaces any current mFragment with new mFragment instance
+            getSupportFragmentManager().beginTransaction().replace(R.id.detail_container, mFragment).commit();
         }
         else {
-            //set arguments for Fragment--this allows you to pass data into the newly created Fragment so it has the data you pass to it, this is for the first time the fragment is created
-            detailFragment.setArguments(getIntent().getExtras());
-            //// DONE: 1/5/2017 this is not working, need to figure out how to add the fragment
+            //set arguments for Fragment--this allows you to pass data into the newly created Fragment so it has the data you pass to it, this is for the first time the mFragment is created
+            mDetailFragment.setArguments(getIntent().getExtras());
+            //// DONE: 1/5/2017 this is not working, need to figure out how to add the mFragment
             //add Fragment to DetailFragment
-            getSupportFragmentManager().beginTransaction().add(R.id.detail_container, detailFragment).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.detail_container, mDetailFragment).commit();
         }
     }
 
-    // DONE: 2/3/2017  save the instance of the Movie Details Fragment so that when the user changes orientation of the screen will get same fragment instance as before to make it seem as if nothing has changed, if so use code in SettingsHostActivity to save fragment instance and recreate it when onCreate() is called again
-    //// DONE: 3/6/2017 this does not save the fragment, fragment is not put into bundle
-    // DONE: 3/7/2017 fragment is not being saved during second device rotation because there is no fragment in the fragment manager?
+    // DONE: 2/3/2017  save the instance of the Movie Details Fragment so that when the user changes orientation of the screen will get same mFragment instance as before to make it seem as if nothing has changed, if so use code in SettingsHostActivity to save mFragment instance and recreate it when onCreate() is called again
+    //// DONE: 3/6/2017 this does not save the mFragment, mFragment is not put into bundle
+    // DONE: 3/7/2017 mFragment is not being saved during second device rotation because there is no mFragment in the mFragment manager?
     @Override
     protected void onSaveInstanceState(Bundle outState){
 
-        //saves fragment instance before fragment is killed
-        getSupportFragmentManager().putFragment(outState,"fragment",getSupportFragmentManager().findFragmentById(R.id.detail_container));
+        //saves mFragment instance before mFragment is killed
+        getSupportFragmentManager().putFragment(outState,"mFragment",getSupportFragmentManager().findFragmentById(R.id.detail_container));
         super.onSaveInstanceState(outState);
     }
 }

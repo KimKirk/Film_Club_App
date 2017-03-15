9781d333239397d9c3dac37d0d067776dbd13f97
KeK <kimberleyEkirk@gmail.com>
2017-03-15 09:51:16 -0700

    remove newInstance method from DetailFragment add final comments to DetailFragment

--- DetailFragment.java
+++ DetailFragment.java
@@ -1,7 +1,6 @@
 package com.spellflight.android.popularmovies;
 
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,46 +12,30 @@ import android.widget.TextView;
  */
 public class DetailFragment extends Fragment{
 
-
-
     // DONE: 3/1/2017 test this method to make sure it works correctly
-    // TODO: 3/6/2017 figure out how this method can be used inside DetailHostActivity in the future so that fragment field setting is independent of activity that host's it
-    //this is a way to set the Fragment's fields so that you don't use the default constructor and then have to call setters
-    public static DetailFragment newInstance(Parcelable movDetParcel){
-        //create new instance of detail fragment
-        DetailFragment detailFrag = new DetailFragment();
-        //create new instance of a Bundle that holds data
-        Bundle arguments = new Bundle();
-        //put index value received into the Bundle and give it a key
-        arguments.putParcelable("movieDetailsParcel",movDetParcel);
-        //set arguments/field for the Detail Fragment
-        detailFrag.setArguments(arguments);
-        //outer bundle = arguments, bundle, parcelable
-        //return the new instance with the set field to the caller
-        return detailFrag;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState){
+        //Fragment instance gets recreated in host Activity with Fragment arguments, so don't need to take saved data and set Views here because the data is set inside Fragment instance with arguments each time Fragment is created
         super.onCreate(savedInstanceState);
-
     }
 
     //DESIGN: possibly override and implement onActivityCreated() where you will set the text for the Fragment's View using the data you received from Intent; do this in this method because need to make sure the View has been initialized for the Fragment before adding text to it
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
+        //use basic functionality from superclass method
         super.onActivityCreated(savedInstanceState);
 
         // DONE: 2/14/2017  get the data from the new instance of Detail Fragment
         // DONE: 2/14/2017  set the data on the views that will show the data
+        //find Views by ID for all TextViews
         TextView titleView = (TextView)getActivity().findViewById(R.id.original_title);
         TextView overvwView = (TextView)getActivity().findViewById(R.id.overview);
         TextView releaseDateView = (TextView)getActivity().findViewById(R.id.release_date);
         TextView voteView = (TextView)getActivity().findViewById(R.id.vote_average);
-        //this goes into the fragment, gets the arguments you set for the fragment, and gets the value at index 0
+        //get the arguments from the Fragment and get the Parcelable object with the given key
         MovieDetailsParcel movieDetailsParcel = getArguments().getParcelable("movieDetails");
         //get field values from inside parcelable object using getters from the Parcelable class you created
-
         titleView.setText(movieDetailsParcel.getmTitle());
         overvwView.setText(movieDetailsParcel.getmOverview());
         releaseDateView.setText("Release Date: " + movieDetailsParcel.getmRelease());
@@ -61,12 +44,12 @@ public class DetailFragment extends Fragment{
     }
 
 
+    //don't need to save text for TextViews because already setting Fragment's arguments each time fragment gets created and those arguments are set inside the Fragment then just use getters from MovieDetailsParcel class  to set text views
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         //inflate the layout and use the View hierarchy from the host Activity to tell AndroidOS where to put the Fragment's View in the host Activity's hierarchy
         return inflater.inflate(R.layout.detail_fragment,container, false);
     }
 
-    //don't need to save text for TextViews because already setting fragment's arguments each time fragment gets created and those arguments are set inside the fragment then just use code to set text views
 
 }

958a6bd83b7f72d40af5afa9c92da409a4b2c3c5
KeK <kimberleyEkirk@gmail.com>
2017-03-24 17:06:55 -0700

    add picasso error handling to MovieImageAdapter

--- MovieImageAdapter.java
+++ MovieImageAdapter.java
@@ -52,7 +52,8 @@ public class MovieImageAdapter extends ArrayAdapter {
         String baseURL = "https://image.tmdb.org/t/p/w500";
 
         //load() gets the data, into() gets the View object to stick data into
-        Picasso.with(getContext()).load(baseURL + "/" + imageUrl).into(imageView);
+        Picasso.with(getContext()).load(baseURL + "/" + imageUrl).placeholder(R.drawable.filmplaceholder).error(R.drawable.picassoimageerror).into(imageView);
+
 
         //return the View back to the caller will be used to put on screen for UI
         return imageView;
